2bd13f09e974b39a28227dd7f394c561688560cc
Renato Soares da Silva <renatosilsoa@gmail.com>
2021-04-11 23:01:52 -0300

    Construção da primeira parte da Page login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import {
-  Container
+  Container,
+  CaixaLogin,
+  ContainerBotaoes,
+  ContainerInputs,
+  Botao,
+  BotaoTexto,
+  InputText,
+  Input
 } from './styles';
-export default function Login( ) {
+export default function Login() {
+
+  const [botaoAtual, setBotaoAtual] = useState('aluno')
+
   return (
     <Container>
-      <Text>Login</Text>
+      <CaixaLogin>
+        <ContainerBotaoes>
+          <Botao ultimoClick={botaoAtual == 'aluno' ? true : false}
+            onPress={() => { setBotaoAtual('aluno') }}>
+            <BotaoTexto ultimoClick={botaoAtual == 'aluno' ? true : false} >Aluno</BotaoTexto>
+          </Botao>
+          <Botao ultimoClick={botaoAtual == 'professor' ? true : false}
+            onPress={() => { setBotaoAtual('professor') }}>
+            <BotaoTexto ultimoClick={botaoAtual == 'professor' ? true : false} >Professor</BotaoTexto>
+          </Botao>
+        </ContainerBotaoes>
+        <ContainerInputs>
+
+        <InputText>Email</InputText>
+        <Input />
+        <InputText>Senha</InputText>
+        <Input />
+
+        </ContainerInputs>
+      </CaixaLogin>
     </Container>
   );
 }
